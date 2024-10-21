306aa86e702f9faa0b7cf9cc91413f196edee185
Trung Bui <thetrung205@gmail.com>
2024-10-21 11:00:02 +0800

    Add Grade

---
+++ src/main/java/tutorlink/gradepackage/Grade.java
@@ -0,0 +1,26 @@
+package tutorlink.gradepackage;
+
+import tutorlink.componentpackage.Component;
+import tutorlink.studentpackage.Student;
+
+public class Grade {
+    private double score;
+    private Component component;
+    private Student student;
+
+    public Grade(Component component, Student student, double score) {
+        this.component = component;
+        this.student = student;
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Grade grade) {
+            return this.score == grade.score
+                    && this.component.equals(grade.component)
+                    && this.student.equals(grade.student);
+        }
+        return false;
+    }
+}
