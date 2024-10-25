831cae3dee0c06080313a346ab187e9a41c2c50a
Trung Bui <thetrung205@gmail.com>
2024-10-25 11:26:28 +0800

    Revert "Add InvalidComponentException"
    This reverts commit 723da9f2fcc86d45ca653ff256e7817e3041fa53.

--- src/main/java/tutorlink/exceptions/InvalidComponentException.java
+++
@@ -1,7 +0,0 @@
-package tutorlink.exceptions;
-
-public class InvalidComponentException extends TutorLinkException {
-    public InvalidComponentException(String message) {
-        super(message);
-    }
-}
